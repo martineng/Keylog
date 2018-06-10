0b3f327156f71b9f1df123685512f84ec53fb7ae
martineng <martin.eca.6651@gmail.com>
2018-06-11 02:22:17 +0800

    Finish FormLogin

--- Keylog/FormLogin.Designer.cs
+++ Keylog/FormLogin.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtbUname = new System.Windows.Forms.TextBox();
+            this.txtboxPwd = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtbUname
+            // 
+            this.txtbUname.Location = new System.Drawing.Point(100, 50);
+            this.txtbUname.MaxLength = 255;
+            this.txtbUname.Name = "txtbUname";
+            this.txtbUname.Size = new System.Drawing.Size(200, 20);
+            this.txtbUname.TabIndex = 0;
+            // 
+            // txtboxPwd
+            // 
+            this.txtboxPwd.Location = new System.Drawing.Point(100, 100);
+            this.txtboxPwd.MaxLength = 255;
+            this.txtboxPwd.Name = "txtboxPwd";
+            this.txtboxPwd.PasswordChar = '*';
+            this.txtboxPwd.ShortcutsEnabled = false;
+            this.txtboxPwd.Size = new System.Drawing.Size(200, 20);
+            this.txtboxPwd.TabIndex = 2;
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.txtboxPwd);
+            this.Controls.Add(this.txtbUname);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormLogin";
@@ -42,10 +64,14 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtbUname;
+        private System.Windows.Forms.TextBox txtboxPwd;
     }
 }
 
