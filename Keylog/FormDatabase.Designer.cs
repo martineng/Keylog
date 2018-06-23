9fc7317e62919f4ac892956ed7fc18bdbd9bcbe4
martineng <martin.eca.6651@gmail.com>
2018-06-24 03:01:16 +0800

    Added new Form to manually input database information

---
+++ Keylog/FormDatabase.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Keylog
+{
+    partial class FormDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tboxIP = new System.Windows.Forms.TextBox();
+            this.tboxUsername = new System.Windows.Forms.TextBox();
+            this.tboxPassword = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(64, 26);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(20, 13);
+            this.lblIP.TabIndex = 0;
+            this.lblIP.Text = "IP:";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(28, 53);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(28, 80);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+            // 
+            // tboxIP
+            // 
+            this.tboxIP.Location = new System.Drawing.Point(91, 23);
+            this.tboxIP.Name = "tboxIP";
+            this.tboxIP.Size = new System.Drawing.Size(171, 20);
+            this.tboxIP.TabIndex = 3;
+            // 
+            // tboxUsername
+            // 
+            this.tboxUsername.Location = new System.Drawing.Point(91, 50);
+            this.tboxUsername.Name = "tboxUsername";
+            this.tboxUsername.Size = new System.Drawing.Size(171, 20);
+            this.tboxUsername.TabIndex = 4;
+            // 
+            // tboxPassword
+            // 
+            this.tboxPassword.Location = new System.Drawing.Point(91, 77);
+            this.tboxPassword.Name = "tboxPassword";
+            this.tboxPassword.Size = new System.Drawing.Size(171, 20);
+            this.tboxPassword.TabIndex = 5;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(117, 113);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // FormDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 145);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tboxPassword);
+            this.Controls.Add(this.tboxUsername);
+            this.Controls.Add(this.tboxIP);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblIP);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormDatabase";
+            this.ShowIcon = false;
+            this.Text = "Database Connection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tboxIP;
+        private System.Windows.Forms.TextBox tboxUsername;
+        private System.Windows.Forms.TextBox tboxPassword;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
